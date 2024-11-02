3f0e11e17526fb22b53ee656755b33938225a245
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-11-02 18:04:27 +0300

    finally commit

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(card_info: str) -> str:
     """
@@ -7,18 +8,16 @@ def mask_account_card(card_info: str) -> str:
     info_list = card_info.split(" ")
     if info_list[0] == "Счет":
 
-
-        return f'Счет {get_mask_account(info_list[1])}'
+        return f"Счет {get_mask_account(info_list[1])}"
 
     else:
 
+        return f"{info_list[0]} {info_list[1]} {get_mask_card_number(info_list[2])}"
 
-        return f'{info_list[0]} {info_list[1]} {get_mask_card_number(info_list[2])}'
 
 def get_date(date: str) -> str:
     date_part = date.split("T")[0]
     year = date_part.split("-")[0]
     month = date_part.split("-")[1]
     day = date_part.split("-")[2]
-    return f'{day}.{month}.{year}'
-
+    return f"{day}.{month}.{year}"
