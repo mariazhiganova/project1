200c939de2376cd79cb7b8652092157e8b964fd0
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-11-10 17:35:02 +0300

    add new function

---
+++ src/processing.py
@@ -0,0 +1,16 @@
+def filter_by_state(info_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Функция, сортирующая списки со словарями данных по ключу 'state'
+    """
+    result = []
+    for dict_ in info_list:
+        if dict_["state"] == state:
+            result.append(dict_)
+
+    return result
+
+
+def sort_by_date(info_list: list[dict], reverse: bool = True) -> list[dict]:
+    info_list.sort(key=lambda x: x["date"], reverse=reverse)
+
+    return info_list
