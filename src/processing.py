e1ad1c10b58e5907b0bf5c4b95763dc71eca2c86
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-11-10 18:58:29 +0300

    formatting by PEP8

--- src/processing.py
+++ src/processing.py
@@ -11,6 +11,9 @@ def filter_by_state(info_list: list[dict], state: str = "EXECUTED") -> list[dict
 
 
 def sort_by_date(info_list: list[dict], reverse: bool = True) -> list[dict]:
+    """
+    Функция, принимающая список словарей и возвращающая отсортированный по параметру "date" в порядке убывания список
+    """
     info_list.sort(key=lambda x: x["date"], reverse=reverse)
 
     return info_list
