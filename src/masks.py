5601a6b29112e8853fd48f28ef037ce18f4ae8d4
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-11-15 01:59:05 +0300

    add new if in function

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,9 @@ def get_mask_card_number(numbers: str) -> str:
     """
     Функция, возвращающая замаскированный номер карты
     """
+    if len(numbers) != 16 or not numbers.isdigit():
+        raise ValueError("Неверный номер карты")
+
     return numbers[:4] + " " + numbers[4:6] + "** ****" + " " + numbers[-4:]
 
 
@@ -9,5 +12,7 @@ def get_mask_account(numbers: str) -> str:
     """
     Функция, возвращающая замаксированный номер счета в формате ** и четырех последних цифр
     """
+    if len(numbers) != 20 or not numbers.isdigit():
+        raise ValueError("Неверный номер счёта")
 
     return f"**{numbers[-4:]}"
