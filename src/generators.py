9cc13c720fa275b11a677be039bc80ea6faaff7e
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-11-25 01:04:10 +0300

    add new functions and tests for these

---
+++ src/generators.py
@@ -0,0 +1,32 @@
+def filter_by_currency(info_list, currency):
+    result_by_currency = (x for x in info_list if x['operationAmount']['currency']['code'] == currency)
+    first_item = next(result_by_currency, None)
+
+    if first_item is None:
+        yield "Операции в заданной валюте не найдены"
+
+    yield first_item
+    yield from result_by_currency
+
+
+def transaction_descriptions(info_list):
+    if len(info_list) == 0:
+        yield "Нет транзакций"
+    for x in info_list:
+        yield x["description"]
+
+
+def card_number_generator(start, stop):
+    for number in range(start, stop + 1):
+
+        number_str = str(number)
+
+        if len(str(number)) < 16:
+            card_number_not_formatted = "0" * (16 - len(str(number))) + str(number)
+        else:
+            card_number_not_formatted = number_str
+
+        card_number = f'{card_number_not_formatted[:4]} {card_number_not_formatted[4:8]} {card_number_not_formatted[8:12]} {card_number_not_formatted[12:]}'
+
+        yield card_number
+
