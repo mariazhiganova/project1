b4db84e81dd1f871a06efd88699c5387bb7c0e24
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-12-10 20:19:42 +0300

    Adding logger in file

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import Optional
 
 
@@ -7,20 +8,42 @@ def get_transaction_data(json_file: Optional[str] = None) -> list:
     Функция, которая принимает путь до JSON-файла
     и возвращает список словарей с данными о финансовых транзакциях.
     """
+    logger = logging.getLogger("utils")
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler("../logs/utils.log", mode="w", encoding="utf-8")
+    file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+
     try:
+        logger.info("Попытка открыть json-файл.")
+
         if json_file is not None:
+            logger.info("Json-файл успешно открывается для чтения.")
+
             with open(json_file, "r", encoding="utf-8") as f:
+                logger.info("Загрузка данных из json-файла и преобразование в Python-объект.")
+
                 transactions_list = json.load(f)
+                logger.info("Проверка: является ли содержимое файла списком.")
+
                 if isinstance(transactions_list, list):
+                    logger.info("Содержимое файла является списком.")
+                    logger.info("Данные в формате Python-объекта получены успешно.")
 
                     return transactions_list
 
+        logger.error("Файл отсутствует")
         return []
 
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as ex:
+        logger.error("Данные из json-файла не соответствует синтаксису JSON или содержат ошибочные данные")
+        logger.error(f"Ошибка {ex}")
+
         return []
 
     except FileNotFoundError:
+        logger.error(f"Файл по пути {json_file} не найден.")
         return []
 
 
