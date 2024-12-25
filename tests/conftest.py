e159b2ac681d70ca8697a4a7db205bfcfa56f3d8
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-12-25 19:59:20 +0300

    Adding new fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -160,33 +160,40 @@ def transactions_list_invalid():
 
 @pytest.fixture
 def transactions_list_without_key():
-    return [[{
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "amount": "43318.34", "currency": {"name": "руб.", "code": "GBP"},
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"},
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "amount": "56883.54", "currency": {"name": "RUB", "code": "RUB"},
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229",
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"},
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657",
-        },
-    ]]
+    return [
+        [
+            {
+                "id": 873106923,
+                "state": "EXECUTED",
+                "date": "2019-03-23T01:09:46.296404",
+                "amount": "43318.34",
+                "currency": {"name": "руб.", "code": "GBP"},
+                "description": "Перевод со счета на счет",
+                "from": "Счет 44812258784861134719",
+                "to": "Счет 74489636417521191160",
+            },
+            {
+                "id": 895315941,
+                "state": "EXECUTED",
+                "date": "2018-08-19T04:27:37.904916",
+                "amount": "56883.54",
+                "currency": {"name": "RUB", "code": "RUB"},
+                "description": "Перевод с карты на карту",
+                "from": "Visa Classic 6831982476737658",
+                "to": "Visa Platinum 8990922113665229",
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+                "amount": "67314.70",
+                "currency": {"name": "руб.", "code": "RUB"},
+                "description": "Перевод организации",
+                "from": "Visa Platinum 1246377376343588",
+                "to": "Счет 14211924144426031657",
+            },
+        ]
+    ]
 
 
 @pytest.fixture
@@ -308,3 +315,38 @@ def excel_data_result():
 @pytest.fixture
 def excel_data_invalid():
     return [1, 2, 3]
+
+
+@pytest.fixture
+def result_transactions_filter():
+    return [
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+    ]
+
+
+@pytest.fixture
+def category_list():
+    return [
+        "Перевод организации",
+        "Перевод с карты на счет",
+        "Перевод с карты на карту",
+        "Перевод со счета на счет",
+        "Открытие вклада",
+    ]
