d219aa6eb0a359db9443c85217b2399c81ed5acb
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-12-25 19:57:23 +0300

    Little fixing and adding tests for that

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -3,10 +3,7 @@ from typing import Optional
 
 
 def get_csv(csv_path: Optional[str]) -> list:
-    """
-    Функция, для считывания финансовых операций из CSV принимает путь к файлу CSV в качестве аргумента
-    и возвращает список словарей с транзакциями.
-    """
+    """ """
     try:
         with open(csv_path, mode="r", encoding="utf-8") as f:
             result_dict = csv.DictReader(f, delimiter=";")
@@ -15,7 +12,3 @@ def get_csv(csv_path: Optional[str]) -> list:
 
     except Exception:
         return []
-
-
-if __name__ == "__main__":
-    print(get_csv("..\\data\\transactions.csv"))
