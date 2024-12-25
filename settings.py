0383389456087643e11b2ca3be366af11946a4ab
Maria Zhiganova <my.nik.mariann@gmail.com>
2024-12-25 21:45:15 +0300

    Delite unused prints

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-print(BASE_DIR)
 
 CSV_PATH = BASE_DIR.joinpath('data/transactions.csv')
 JSON_PATH = BASE_DIR.joinpath('data/operations.json')
@@ -9,7 +8,3 @@ XLSX_PATH = BASE_DIR.joinpath('data/transactions.xlsx')
 
 LOG_MASKS_PATH = BASE_DIR.joinpath('logs/masks.log')
 LOG_UTILS_PATH = BASE_DIR.joinpath('logs/utils.log')
-
-print(CSV_PATH)
-print(JSON_PATH)
-print(XLSX_PATH)
